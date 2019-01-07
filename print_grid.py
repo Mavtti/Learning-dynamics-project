24b78f443b42ed4eab971c3c541414adc287e764
mvegetti <44265860+Mavtti@users.noreply.github.com>
2019-01-07 07:29:04 -0800

    Add files via upload

--- print_grid.py
+++ print_grid.py
@@ -17,9 +17,10 @@ def getGrid(fichier):
     grid = []
     with open(fichier, 'rt') as f:
         for line in f:
-            grid.insert(0,literal_eval(line[:-1])) #Opposite order
+            grid.append(literal_eval(line[:-1])) #Opposite order
     return grid
 
+grid = getGrid("grid.txt")
 def vertical_wall(walls):
     return walls & 1
 
@@ -37,10 +38,7 @@ def printGrid(grid):
     ax.axes.get_yaxis().set_ticklabels([])
 
     ax2 = ax.twinx()
-    #ax2.xaxis.set_ticks([float(x)/2 for x in range(2*n)])
-    #ax2.yaxis.set_ticks([float(x)/2 for x in range(2*m+1)])
     ax3 = ax.twiny()
-    #ax3.xaxis.set_ticks([float(x)/2 for x in range(2*n+1)])
 
     # create a grid
     ax.grid(True, linestyle='-', linewidth=1)
@@ -54,6 +52,7 @@ def printGrid(grid):
                 ax.axvline(x=j,ymin=float(i)/m,ymax=float(i+1)/m,linewidth=4, color='k')
             if horizontal_wall(walls):
                 ax.axhline(y=i,xmin=float(j)/n,xmax=float(j+1)/n,linewidth=4, color='k')
+            
             if flag:
                 ax.text(float(j+0.5)/n, float(i+0.5)/m, flag, fontsize=12,horizontalalignment='center',
                         verticalalignment='center',
@@ -71,31 +70,12 @@ def printGrid(grid):
                         verticalalignment='center',
                         transform=ax.transAxes)
 
-    ax.text(float(4.15)/n, float(7+0.5)/m, 'S1', fontsize=10,
-                        weight='bold',
-                        verticalalignment='center',
-                        transform=ax.transAxes)
-    ax.axhline(y=m,linewidth=4,color='k')
-    ax.axvline(x=n,linewidth=4,color='k')
-    ax.text(float(14.20)/n, float(7+0.5)/m, 'S2', fontsize=10,
-                        weight='bold',
-                        verticalalignment='center',
-                        transform=ax.transAxes)
-    ax.text(float(0.8)/n, float(1.5)/m, 'Goal', fontsize=8,
-                        weight='bold',
-                        rotation=-45,
-                        verticalalignment='center',
-                        transform=ax.transAxes)
-    ax.axhline(y=m,linewidth=4,color='k')
-    ax.axvline(x=n,linewidth=4,color='k')
-    ax.axhline(y=m,linewidth=4,color='k')
-    ax.axvline(x=n,linewidth=4,color='k')
-    ax2.set_yticklabels([], minor=False)
-    ax3.set_xticklabels([],minor=False)
-    ax2.tick_params(axis=u'both', which=u'both',length=0)
-    ax3.tick_params(axis=u'both', which=u'both',length=0)
-    ax2.yaxis.set_ticks_position('left')
-    ax3.xaxis.set_ticks_position('bottom')
 
+    ax.text(float(4.15)/n, float(7.5)/m, 'S1', fontsize=10, weight='bold', verticalalignment='center', transform=ax.transAxes)
+    ax.text(float(14.15)/n, float(7.5)/m, 'S2', fontsize=10, weight='bold', verticalalignment='center', transform=ax.transAxes)
+    ax.text(float(0.9)/n, float(1.5)/m, 'Goal', fontsize=8, weight='bold', rotation=-45, verticalalignment='center', transform=ax.transAxes)
+
+    ax.axhline(y=m,linewidth=4,color='k')
+    ax.axvline(x=n,linewidth=4,color='k')
     plt.draw()
     plt.show()
